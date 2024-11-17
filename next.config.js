02d470ab262639acbd19ea801a7abd9d9c358c45
Amankumar <amanks2580@gmail.com>
2024-11-18 03:17:28 +0530

    deployingg

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  basePath: '/whatbytes',
-  assetPrefix: '/whatbytes/',
-  output: 'export', // Ensure it's using the new export option for static sites
-  experimental: {
-    appDir: true, // Make sure App Directory is enabled for static export
-  },
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
