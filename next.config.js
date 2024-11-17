ed21c9b921d40d64adc4671c9f94d9488e5d1b0e
Amankumar <amanks2580@gmail.com>
2024-11-18 03:13:20 +0530

    deployingg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    basePath: '/whatbytes',
-    assetPrefix: '/whatbytes/',
-    output: 'export',
-  };
-  
+  basePath: '/whatbytes',
+  assetPrefix: '/whatbytes/',
+  output: 'export', // Ensure it's using the new export option for static sites
+  experimental: {
+    appDir: true, // Make sure App Directory is enabled for static export
+  },
+};
